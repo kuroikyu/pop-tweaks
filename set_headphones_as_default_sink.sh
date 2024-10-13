51d0bd96c1381959e4df2dda050e01f8ea8ba687
Kuroi Kyu <hello@kuroikyu.com>
2024-10-13 11:24:06 +0200

    batman

---
+++ set_headphones_as_default_sink.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Function to get the default sink number
+get_default_sink() {
+  wpctl status | grep -A 2 'Sinks' | grep 'Scarlett Solo (3rd Gen.) Analog Stereo' | awk '{print $2}' | cut -d'.' -f1 | head -n 1
+}
+
+# Get the default sink number
+sink_number=$(get_default_sink)
+
+# Set the default sink
+if [[ -n "$sink_number" ]]; then
+  wpctl set-default "$sink_number"
+  echo "Default sink set to $sink_number"
+else
+  echo "Could not find the 'Scarlett Solo (3rd Gen.) Analog Stereo' device."
+fi
---
+++ set_speakers_as_default_sink.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Function to get the default sink number
+get_default_sink() {
+  wpctl status | grep -A 2 'Sinks' | grep 'PCM2704 16-bit stereo audio DAC Analog Stereo' | awk '{print $2}' | cut -d'.' -f1 | head -n 1
+}
+
+# Get the default sink number
+sink_number=$(get_default_sink)
+
+# Set the default sink
+if [[ -n "$sink_number" ]]; then
+  wpctl set-default "$sink_number"
+  echo "Default sink set to $sink_number"
+else
+  echo "Could not find the 'PCM2704 16-bit stereo audio DAC Analog Stereo' device."
+fi
